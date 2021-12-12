84baec46116b32fda129659bd1d7555e24fa0b63
chihong <c.hong@tudelft.nl>
2021-12-12 21:54:33 +0100

    .

--- federated_learning/arguments.py
+++ federated_learning/arguments.py
@@ -21,7 +21,7 @@ class Arguments:
     def __init__(self, logger):
         self.logger = logger
 
-        self.dataset = "cifar_10"
+        self.dataset = "cifar_10"  # "cifar_10" "fashion_mnist"
         self.batch_size = 10
         self.test_batch_size = 1000
         self.epochs = 100
